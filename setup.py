f024e0eadb9f3d5d7ffa5d860d081469d4a7d740
Locyst <154724542+Locyst@users.noreply.github.com>
2024-02-17 20:20:23 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='LocystEncoder',
+    packages=find_packages(include=['LocystEncoder']),
+    version='0.0.1',
+    description='A python library created for the encoding of python strings. This should not be used for secure things.',
+    author='Locyst',
+)
